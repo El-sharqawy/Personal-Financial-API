daa0c70d72b4a7c4ecf28d1375ffa2e5515f201b
El-sharqawy <elsharqawy2@gmail.com>
2024-10-12 16:08:27 +0300

    feat: implemented user views implemented user views classes that allows to view user details.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from .models import CustomUser
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+
+class UserDetailView(generics.RetrieveUpdateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+class UserBalanceView(APIView):
+    queryset = CustomUser.objects.all()
+
+    def get(self, request):
+        return Response({"balance": request.user.current_balance})
