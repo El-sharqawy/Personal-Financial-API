9cabe579f7c3d6ff0c024604b731d45b51777a6c
El-sharqawy <elsharqawy2@gmail.com>
2024-10-12 16:26:28 +0300

    feat: implement models classes - created IncomeSource class - created Expense class reason: track user expenses and income

--- expensetracker/models.py
+++ expensetracker/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from users.models import CustomUser
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class IncomeSource(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="income_sources")
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.user.username}"
+
+class Expense(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="expanses")
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name} - {self.category} - {self.user.username}"
