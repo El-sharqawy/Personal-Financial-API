53a6d140a4843c16e92fe8207eb0b19a9c721864
El-sharqawy <elsharqawy2@gmail.com>
2024-10-13 15:41:29 +0300

    feat: create views and implement income source views

--- expensetracker/views.py
+++ expensetracker/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.db.models import QuerySet, Sum
+from .models import IncomeSource, Expense, Transaction
+from users.models import CustomUser
+from .serializers import IncomeSourceSerializer, ExpenseSerializer, TransactionSerializer
 
-# Create your views here.
+class IncomeSourceListCreateView(generics.ListCreateAPIView):
+    serializer_class = IncomeSourceSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return IncomeSource.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class IncomeSourceDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = IncomeSourceSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return IncomeSource.objects.filter(user=self.request.user)
