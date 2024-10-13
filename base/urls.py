16aba49c30907bebfa9c2ae8bee14208eaa2c46b
El-sharqawy <elsharqawy2@gmail.com>
2024-10-13 17:50:58 +0300

    ci: added swagger docs to the API - created user and added his balance

--- base/urls.py
+++ base/urls.py
@@ -15,10 +15,30 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Personal Financial API",
+      default_version='v1',
+      description="A Financial API that allows you to track your income and expenses and calcualte it",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="elsharqawy2@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('tracker/', include('expensetracker.urls')),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
