2640cea35447ebea51af31f7dec62ac7f227b78c
El-sharqawy <elsharqawy2@gmail.com>
2024-10-13 15:44:38 +0300

    ci: implement expenses create and detail view

--- expensetracker/views.py
+++ expensetracker/views.py
@@ -23,3 +23,20 @@ class IncomeSourceDetailView(generics.RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         return IncomeSource.objects.filter(user=self.request.user)
+
+class ExpenseListCreateView(generics.ListCreateAPIView):
+    serializer_class = ExpenseSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Expense.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class ExpenseDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ExpenseSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Expense.objects.filter(user=self.request.user)
